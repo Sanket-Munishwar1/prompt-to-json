5792f142e7ac6ab6748d4c7f02ff09c54f0e2f40
Simran Yadav <simranyadav464@gmail.com>
2024-02-27 18:05:26 +0530

    added the slide15

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { prosCons1 } from "../controllers/pros_cons1.js"
 import { Number1 } from "../controllers/Number1.js"
 import { Number2 } from "../controllers/Number2.js"
 import { Number3 } from "../controllers/Number3.js"
+import { Number4 } from "../controllers/Number4.js"
 
 const router = express.Router();
 
@@ -11,5 +11,6 @@ router.post("/pros_cons1", prosCons1);
 router.post("/number1", Number1);
 router.post("/number2", Number2);
 router.post("/number3", Number3);
+router.post("/number4", Number4);
 
 export default router;
