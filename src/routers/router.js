68e075ea3b74748b0324c0d936a5d761a20831ee
Simran Yadav <simranyadav464@gmail.com>
2024-02-27 19:56:10 +0530

    Added the slide30

--- src/routers/router.js
+++ src/routers/router.js
@@ -4,6 +4,8 @@ import { box2 } from "../controllers/box2.js";
 import { box3 } from "../controllers/box3.js";
 import { box4 } from "../controllers/box4.js";
 import { box5 } from "../controllers/box5.js";
+import { box6 } from "../controllers/box6.js";
+import { box7 } from "../controllers/box7.js";
 
 const router = express.Router();
 
@@ -13,5 +15,7 @@ router.post("/box2", box2);
 router.post("/box3", box3);
 router.post("/box4", box4);
 router.post("/box5", box5);
+router.post("/box6", box6);
+router.post("/box7", box7);
 
 export default router;
