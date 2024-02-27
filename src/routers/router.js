619a3195f729b7fb8c00c4b2275e1cbbd7a0f5a8
Simran Yadav <simranyadav464@gmail.com>
2024-02-27 17:15:15 +0530

    added the code of slide9

--- src/routers/router.js
+++ src/routers/router.js
@@ -3,6 +3,7 @@ import { differentType1 } from "../controllers/different_type1.js";
 import { differentType2 } from "../controllers/different_type2.js";
 import { differentType3 } from "../controllers/different_type3.js";
 import { differentType4 } from "../controllers/different_type4.js";
+import { differentType5 } from "../controllers/different_type5.js";
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.post("/different-type1", differentType1);
 router.post("/different-type2", differentType2);
 router.post("/different-type3", differentType3);
 router.post("/different-type4", differentType4);
+router.post("/different-type5", differentType5);
 
 export default router;
