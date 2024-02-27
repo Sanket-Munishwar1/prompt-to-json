8f9ef4b3494871ccf6c3e81a9d8c33d38b62a8b8
Simran Yadav <simranyadav464@gmail.com>
2024-02-27 17:00:33 +0530

    Added the code for slide7

--- src/routers/router.js
+++ src/routers/router.js
@@ -5,6 +5,7 @@ import { Logos2 } from "../controllers/Logos2.js"
 import { Logos3 } from "../controllers/Logos3.js"
 import { Logos4 } from "../controllers/Logos4.js"
 import { Logos5 } from "../controllers/Logos5.js"
+import { Logos6 } from "../controllers/Logos6.js"
 
 const router = express.Router();
 
@@ -15,5 +16,6 @@ router.post("/logos2", Logos2);
 router.post("/logos3", Logos3);
 router.post("/logos4", Logos4);
 router.post("/logos5", Logos5);
+router.post("/logos6", Logos6);
 
 export default router;
