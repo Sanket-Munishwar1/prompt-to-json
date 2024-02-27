a92b6b1cd643fdec838cf0d0f2a743a951290b02
Simran Yadav <simranyadav464@gmail.com>
2024-02-27 16:48:46 +0530

    Added the slide3

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { prosCons1 } from "../controllers/pros_cons1.js"
 import { Logos1 } from "../controllers/Logos1.js"
+import { Logos2 } from "../controllers/Logos2.js"
 
 const router = express.Router();
 
 // Pros_cons -------------------------------------
 router.post("/pros_cons1", prosCons1);
 router.post("/logos1", Logos1);
+router.post("/logos2", Logos2);
 
 export default router;
