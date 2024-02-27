d32f4e4d4b6dc7caf59eec642f2444a33d52fbcd
Simran Yadav <simranyadav464@gmail.com>
2024-02-27 18:07:02 +0530

    added the slide

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,5 +1,4 @@
-import express from "express";
-import { prosCons1 } from "../controllers/pros_cons1.js"
+
 import { Logos1 } from "../controllers/Logos1.js"
 import { Logos2 } from "../controllers/Logos2.js"
 import { Logos3 } from "../controllers/Logos3.js"
@@ -10,7 +9,6 @@ import { Logos6 } from "../controllers/Logos6.js"
 const router = express.Router();
 
 // Pros_cons -------------------------------------
-router.post("/pros_cons1", prosCons1);
 router.post("/logos1", Logos1);
 router.post("/logos2", Logos2);
 router.post("/logos3", Logos3);
