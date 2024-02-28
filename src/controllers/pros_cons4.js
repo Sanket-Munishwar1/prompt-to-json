7d5c10565dd2c9235e578eb8379ad051a4e4eb11
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2024-02-28 10:40:49 +0530

    new

--- src/controllers/pros_cons4.js
+++ src/controllers/pros_cons4.js
@@ -8,8 +8,8 @@ function createPrompt(slideTitle, slideDesc) {
 d) 'cons' – string array of 3 points, where each point should be a detailed paragraph of 1 lines and in between 10-12 words, covering negative or cons part of specific information or examples relevant to the slide's topic. Do not leave a trailing comma after the last item in this array.
 e) 'consTitle' – string of 2-3 words and 1 line covering title of negative or cons part of information.
 The output should be only the Valid JSON object, without any extraneous text or explanation.JSON:.
-f)'prosimage1' - a string keyword related to the subtitle. This will be used for image search on google keep it short.
-g)'consimage1' - a string keyword related to the subtitle. This will be used for image search on google keep it short.`
+f)'subtitle1' - string of 2-3 words and 1 line covering title of negative or cons part of information.
+g)'subtitle2' - string of 2-3 words and 1 line covering title of negative or cons part of information.`
 
 
   return prompt;
@@ -63,8 +63,8 @@ export async function prosCons4(req, res) {
     let cons1 = parsedJson.cons[0];
     let cons2 = parsedJson.cons[1];
     let cons3 = parsedJson.cons[2];
-    let prosimage1 = parsedJson.prosimage1;
-    let consimage1 = parsedJson.consimage1;
+    let subtitle1 = parsedJson.subtitle1;
+    let subtitle2 = parsedJson.subtitle2;
 
     if (presentationTitle === undefined || presentationTitle === "" 
     || prosTitle === undefined || prosTitle === "" 
@@ -75,8 +75,8 @@ export async function prosCons4(req, res) {
     || cons1 === undefined || cons1 === "" 
     || cons2 === undefined || cons2 === ""
     || cons3 === undefined || cons3 === ""
-    || prosimage1 === undefined || prosimage1 === "" 
-    || consimage1 === undefined || consimage1 === "" ) 
+    || subtitle1 === undefined || subtitle1 === "" 
+    || subtitle2 === undefined || subtitle2 === "" ) 
     {
       return res.status(500).json({
         status: "error",
@@ -98,8 +98,8 @@ export async function prosCons4(req, res) {
         parsedJson.cons[2] ? parsedJson.cons[2] : "",
       ],
       "consTitle": parsedJson.consTitle ? parsedJson.consTitle : "",
-      "prosimage1": parsedJson.prosimage1 ? parsedJson.prosimage1 : "",
-      "consimage1": parsedJson.consimage1 ? parsedJson.consimage1 : "",
+      "subtitle1": parsedJson.subtitle1 ? parsedJson.subtitle1 : "",
+      "subtitle2": parsedJson.subtitle2 ? parsedJson.subtitle2 : "",
     }
 
     return res.status(200).json({
@@ -119,104 +119,102 @@ export async function prosCons4(req, res) {
 
 
     
-  
-      // Add objects to the second Slide, you can define your options here
-  
-    //   let opts = {
-    //       x: "3%",
-    //       y: "50px",
-    //       w: '100%',
-    //       h: 1,
-    //       align: 'Left',
-    //       fontSize: 24,
-    //       color: '000000',
-    //       bold:true
-    //   };
-  
-    //   slide.addText(
-    //       'Indian History',
-    //       opts
-    //   );
-      
-    //   slide.addShape(pptx.shapes.OVAL, { x: '24%', y: '19%', w: '5%', h: 0.5, line: { color: '0000ff', width: 1 } , fill: { color: 'ffffff' } });
-      
-      
-    //   slide.addShape(pptx.shapes.OVAL, { x: '72%', y: '19%', w: '5%', h: 0.5, line: { color: 'ff0000', width: 1 } , fill: { color: 'ffffff' } });
-      
-    //   slide.addImage({
-    //       path: "https://img.icons8.com/?size=32&id=77258&format=png",
-    //       h: "5%",
-    //       w: "3%",
-    //       x: "25%",
-    //       y: "21%"
-    //   }); 
-      
-    //   slide.addImage({
-    //       path: "https://img.icons8.com/?size=32&id=77258&format=png",
-    //       h: "5%",
-    //       w: "3%",
-    //       x: "73%",
-    //       y: "21%"
-    //   }); 
-      
-  
-  
-    //   //Subtitle
-    //   slide.addText(
-    //       'Pros',
-    //       { x: "24%", y: "25%", w: '100%', h: 1, align: 'Left', fontSize: 14, color: '0000ff',bold:true }
-    //   )
-  
-    //   slide.addText(
-    //       'Cons',
-    //       { x: "72%", y: "25%", w: '100%', h: 1, align: 'Left', fontSize: 14, color: 'ff0000',bold:true }
-    //   )
-  
-    //   //Pros info
-  
-    //   slide.addShape(pptx.shapes.OVAL, { x: '6%', y: '42%', w: '0.5%', h: 0.05, line: { color: '0000ff', width: 1 } , fill: { color: '0000ff' } });
-  
-    //   slide.addText(
-    //       "Indian Army's modernization efforts are on track with the induction of advanced weaponry and technology.",
-    //       { x: "8%", y: "35%", w: '40%', h: 1, fontSize: 11, color: '000000' }
-    //   )
-  
-    //   slide.addShape(pptx.shapes.OVAL, { x: '6%', y: '52%', w: '0.5%', h: 0.05, line: { color: '0000ff', width: 1 } , fill: { color: '0000ff' } });
-  
-    //   slide.addText(
-    //       "Increased focus on cybersecurity to combat emerging threats in the digital age.",
-    //       { x: "8%", y: "45%", w: '40%', h: 1, fontSize: 11, color: '000000' }
-    //   )
-  
-    //   slide.addShape(pptx.shapes.OVAL, { x: '6%', y: '62%', w: '0.5%', h: 0.05, line: { color: '0000ff', width: 1 } , fill: { color: '0000ff' } });
-  
-    //   slide.addText(
-    //       "Increased focus on cybersecurity to combat emerging threats in the digital age.",
-    //       { x: "8%", y: "55%", w: '40%', h: 1, fontSize: 11, color: '000000' }
-    //   )
-  
-    //   //Cons info
-  
-    //   slide.addShape(pptx.shapes.OVAL, { x: '54%', y: '42%', w: '0.5%', h: 0.05, line: { color: 'ff0000', width: 1 } , fill: { color: 'ff0000' } });
-  
-    //   slide.addText(
-    //       'Challenges in border security due to ongoing territorial disputes with neighboring countries.',
-    //       { x: "56%", y: "35%", w: '40%', h: 1, fontSize: 11, color: '000000' }
-    //   )
-  
-    //   slide.addShape(pptx.shapes.OVAL, { x: '54%', y: '52%', w: '0.5%', h: 0.05, line: { color: 'ff0000', width: 1 } , fill: { color: 'ff0000' } });
-  
-    //   slide.addText(
-    //       'Budget constraints impacting the pace of infrastructure development and capacity building.',
-    //       { x: "56%", y: "45%", w: '40%', h: 1, fontSize: 11, color: '000000' }
-    //   )
-  
-    //   slide.addShape(pptx.shapes.OVAL, { x: '54%', y: '62%', w: '0.5%', h: 0.05, line: { color: 'ff0000', width: 1 } , fill: { color: 'ff0000' } });
-  
-    //   slide.addText(
-    //       'Budget constraints impacting the pace of infrastructure development and capacity building.',
-    //       { x: "56%", y: "55%", w: '40%', h: 1, fontSize: 11, color: '000000' }
-    //   )
-  
-  
-  
+// let opts = {
+//   x: "3%",
+//   y: "50px",
+//   w: '100%',
+//   h: 1,
+//   align: 'Left',
+//   fontSize: 24,
+//   color: '000000',
+//   bold:true
+// };
+
+// slide.addText(
+//   'Indian History',
+//   opts
+// );
+
+// slide.addShape(pptx.shapes.OVAL, { x: '24%', y: '19%', w: '5%', h: 0.5, line: { color: '0000ff', width: 1 } , fill: { color: 'ffffff' } });
+
+
+// slide.addShape(pptx.shapes.OVAL, { x: '72%', y: '19%', w: '5%', h: 0.5, line: { color: 'ff0000', width: 1 } , fill: { color: 'ffffff' } });
+
+// slide.addText(
+// '↗',
+// {h: "5%",
+// w: "3%",
+// x: "24.7%",
+// y: "21%",
+// color: '0000ff',
+// bold:true }
+// )
+
+// slide.addText(
+// '↘',
+// {h: "5%",
+// w: "3%",
+// x: "72.5%",
+// y: "21%",
+// color: 'ff0000',
+// bold:true }
+// )
+
+
+// //Subtitle
+// slide.addText(
+//   'Pros',
+//   { x: "24%", y: "25%", w: '100%', h: 1, align: 'Left', fontSize: 14, color: '0000ff',bold:true }
+// )
+
+// slide.addText(
+//   'Cons',
+//   { x: "72%", y: "25%", w: '100%', h: 1, align: 'Left', fontSize: 14, color: 'ff0000',bold:true }
+// )
+
+// //Pros info
+
+// slide.addShape(pptx.shapes.OVAL, { x: '6%', y: '42%', w: '0.5%', h: 0.05, line: { color: '0000ff', width: 1 } , fill: { color: '0000ff' } });
+
+// slide.addText(
+//   "Indian Army's modernization efforts are on track with the induction of advanced weaponry and technology.",
+//   { x: "8%", y: "35%", w: '40%', h: 1, fontSize: 11, color: '000000' }
+// )
+
+// slide.addShape(pptx.shapes.OVAL, { x: '6%', y: '52%', w: '0.5%', h: 0.05, line: { color: '0000ff', width: 1 } , fill: { color: '0000ff' } });
+
+// slide.addText(
+//   "Increased focus on cybersecurity to combat emerging threats in the digital age.",
+//   { x: "8%", y: "45%", w: '40%', h: 1, fontSize: 11, color: '000000' }
+// )
+
+// slide.addShape(pptx.shapes.OVAL, { x: '6%', y: '62%', w: '0.5%', h: 0.05, line: { color: '0000ff', width: 1 } , fill: { color: '0000ff' } });
+
+// slide.addText(
+//   "Increased focus on cybersecurity to combat emerging threats in the digital age.",
+//   { x: "8%", y: "55%", w: '40%', h: 1, fontSize: 11, color: '000000' }
+// )
+
+// //Cons info
+
+// slide.addShape(pptx.shapes.OVAL, { x: '54%', y: '42%', w: '0.5%', h: 0.05, line: { color: 'ff0000', width: 1 } , fill: { color: 'ff0000' } });
+
+// slide.addText(
+//   'Challenges in border security due to ongoing territorial disputes with neighboring countries.',
+//   { x: "56%", y: "35%", w: '40%', h: 1, fontSize: 11, color: '000000' }
+// )
+
+// slide.addShape(pptx.shapes.OVAL, { x: '54%', y: '52%', w: '0.5%', h: 0.05, line: { color: 'ff0000', width: 1 } , fill: { color: 'ff0000' } });
+
+// slide.addText(
+//   'Budget constraints impacting the pace of infrastructure development and capacity building.',
+//   { x: "56%", y: "45%", w: '40%', h: 1, fontSize: 11, color: '000000' }
+// )
+
+// slide.addShape(pptx.shapes.OVAL, { x: '54%', y: '62%', w: '0.5%', h: 0.05, line: { color: 'ff0000', width: 1 } , fill: { color: 'ff0000' } });
+
+// slide.addText(
+//   'Budget constraints impacting the pace of infrastructure development and capacity building.',
+//   { x: "56%", y: "55%", w: '40%', h: 1, fontSize: 11, color: '000000' }
+// )
+
