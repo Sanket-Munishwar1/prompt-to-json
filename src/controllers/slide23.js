4bbd01c55f3a32c20744629edc8ad9427f39fcdd
Simran Yadav <simranyadav464@gmail.com>
2024-02-29 15:02:03 +0530

    added the slide23

--- src/controllers/slide23.js
+++ src/controllers/slide23.js
@@ -1,3 +1,126 @@
+import { createAskAIPayload, createOptions, callOpenAI } from "../utils/helper.js"
+
+function createPrompt(slideTitle, slideDesc) {
+  const prompt = `Craft a JSON for a presentation slide object with ${slideTitle} and slide description: ${slideDesc} should have:
+  a) 'title' – a short, catchy headline summarizing the slide's content in between 4-5 words.
+  b) 'subTitle1' – give the two-three digit number relavant to slide's topic
+  c) 'subTitle2' – give the two-three digit number relavant to slide's topic
+  d) 'subTitle3' – give the two-three digit number relavant to slide's topic
+  e) 'subTitle4' – give the two-three digit number relavant to slide's topic
+  f) 'subTitle5' – give the two-three digit number relavant to slide's topic
+  g) 'subTitle6' – give the two-three digit number relavant to slide's topic
+  h) 'info1' – string of 1 words and 3 line covering title of positive or Pros part of information.
+  i) 'info2' – string of 1 words and 3 line covering title of positive or Pros part of information.
+  j) 'info3' – string of 1 words and 3 line covering title of positive or Pros part of information.
+  k) 'info4' – string of 1 words and 3 line covering title of positive or Pros part of information.
+  l) 'info5' – string of 1 words and 3 line covering title of positive or Pros part of information.
+  m) 'info6' – string of 1 words and 3 line covering title of positive or Pros part of information.`;
+
+  return prompt;
+}
+
+export async function differentType2(req, res) {
+  try {
+    let { slideTitle, slideDesc, plan } = req.body;
+
+    if (!slideTitle || !slideDesc || !plan) {
+      return res.status(500).json({
+        status: "error",
+        message: "Something is missing"
+      })
+    }
+
+    const prompt = createPrompt(slideTitle, slideDesc);
+    console.log("Prompt: ", prompt)
+    const payload = createAskAIPayload(prompt, plan);
+    const options = createOptions(payload);
+    let { response, result, tokenUsed, error } = await callOpenAI(options);
+
+    if (error) {
+      let counter = 0;
+      for (let i = 0; i < 3; i++) {
+        let { response1, result1, tokenUsed1, error1 } = callOpenAI(options);
+        if (error1) {
+          // Logger.log(error in OpenAI calling loop ${counter} : ${error1} at procons_bullet);
+          counter++;
+          if (i === 2) {
+
+            return { "success": false, "message": `error while callingOpenAI ${error1}` };
+          }
+        } else {
+          result = result1;
+          tokenUsed = tokenUsed1;
+          break;
+        }
+      }
+    }
+
+    const parsedJson = JSON.parse(result);
+    console.log("The JSON is valid.");
+
+    let presentationTitle = parsedJson.title;
+    let subTitle1 = parsedJson.subTitle1;
+    let subTitle2 = parsedJson.subTitle2;
+    let subTitle3 = parsedJson.subTitle3;
+    let subTitle4 = parsedJson.subTitle4;
+    let subTitle5 = parsedJson.subTitle5;
+    let subTitle6 = parsedJson.subTitle6;
+    let info1 = parsedJson.info1;
+    let info2 = parsedJson.info2;
+    let info3 = parsedJson.info3;
+    let info4 = parsedJson.info4;
+    let info5 = parsedJson.info5;
+    let info6 = parsedJson.info6;
+
+    if (presentationTitle === undefined || presentationTitle === "" || 
+    subTitle1 === undefined || subTitle1 === "" || 
+    subTitle2 === undefined || subTitle2 === "" || 
+    subTitle3 === undefined || subTitle3 === "" || 
+    subTitle4 === undefined || subTitle4 === "" || 
+    subTitle5 === undefined || subTitle5 === "" || 
+    subTitle6 === undefined || subTitle6 === "" || 
+    info1 === undefined || info1 === "" || 
+    info2 === undefined || info2 === "" || 
+    info3 === undefined || info3 === "" || 
+    info4 === undefined || info4 === "" ||
+    info5 === undefined || info5 === "" || 
+    info6 === undefined || info6 === "") {
+
+      return res.status(500).json({
+        status: "error",
+        message: "Something is missing"
+      })
+    }
+
+    var customJSON = {
+      "title": parsedJson.title ? parsedJson.title : slideTitle,
+      "subTitle1": parsedJson.subTitle1 ? parsedJson.subTitle1 : "",
+      "subTitle2": parsedJson.subTitle2 ? parsedJson.subTitle2 : "",
+      "subTitle3": parsedJson.subTitle3 ? parsedJson.subTitle3 : "",
+      "subTitle4": parsedJson.subTitle4 ? parsedJson.subTitle4 : "",
+      "subTitle5": parsedJson.subTitle5 ? parsedJson.subTitle5 : "",
+      "subTitle6": parsedJson.subTitle6 ? parsedJson.subTitle6 : "",
+      "info1": parsedJson.info1 ? parsedJson.info1 : "",
+      "info2": parsedJson.info2 ? parsedJson.info2 : "",
+      "info3": parsedJson.info3 ? parsedJson.info3 : "",
+      "info4": parsedJson.info4 ? parsedJson.info4 : "",
+      "info5": parsedJson.info5 ? parsedJson.info5: "",
+      "info6": parsedJson.info6 ? parsedJson.info6 : ""
+    }
+
+    return res.status(200).json({
+      status: "success",
+      message: "JSON generated Successfully",
+      json: customJSON
+    })
+  } catch (error) {
+    console.log("Error: ", error);
+    return res.status(500).json({
+      status: "error",
+      message: `Internal server error, ${error}`
+    })
+  }
+}
 
 
 
