e721b146755861dcc44677c9a27a2c3fc2dd5a7a
Simran Yadav <simranyadav464@gmail.com>
2024-02-29 12:18:22 +0530

    Updated the code

--- src/routers/router.js
+++ src/routers/router.js
@@ -3,7 +3,7 @@ import { Number1 } from "../controllers/Number1.js"
 import { Number2 } from "../controllers/Number2.js"
 import { Number3 } from "../controllers/Number3.js"
 import { Number4 } from "../controllers/Number4.js"
-import { Number5 } from "../controllers/Number5.js"
+
 
 const router = express.Router();
 
@@ -13,6 +13,6 @@ router.post("/number1", Number1);
 router.post("/number2", Number2);
 router.post("/number3", Number3);
 router.post("/number4", Number4);
-router.post("/number5", Number5);
+
 
 export default router;
